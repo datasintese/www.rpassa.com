e867e731616a115c2e4b0a2b4532309b992aa8a6
Guilherme Alencar <47083279+grilo88@users.noreply.github.com>
2022-01-16 22:50:42 -0300

    Newsletter, Pesquisa de Carros, etc.

---
+++ bll/rodape.js
@@ -0,0 +1,35 @@
+(function ($) {
+    'use strict';
+
+    $('#newsletter').submit(function (event) {
+        event.preventDefault();
+
+        $.ajax({
+            url: sessionStorage.getItem("api") + "/v1/newsletter",
+            type: "POST", cache: false, async: false, dataType: "json",
+            data: {
+                Email: $("#emailsubscribe").val(),
+            },
+            contentType: "application/x-www-form-urlencoded; charset=utf-8",
+            success: function (result, textStatus, request) {
+                try {
+                    Mensagem(result.mensagem, 'success');
+                } catch (error) {
+                    Mensagem(JSON.stringify(result), 'success');
+                }
+                $("#newsletter").trigger("reset");
+            },
+            error: function (request, textStatus, errorThrown) {
+                if (!MensagemErroAjax(request, errorThrown)) {
+                    try {
+                        var obj = $.parseJSON(request.responseText)
+                        Mensagem(obj.mensagem, 'warning', function () { $("#emailsubscribe").select(); });
+                    } catch (error) {
+                        Mensagem(request.responseText, 'warning', function () { $("#emailsubscribe").select(); });
+                    }
+                }
+            }
+        }
+        );
+    });
+})(jQuery);
