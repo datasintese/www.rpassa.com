8990920e078a8c538fcd1de99caabca799cd7c41
Maike <43520476+Maikedef@users.noreply.github.com>
2022-01-31 16:43:29 -0300

    **Html** - Atualizado detalhes-produto.html link do usuário incluido. - Atualizado usuario.js organizado parte do perfil.

--- bll/usuario.js
+++ bll/usuario.js
@@ -1,13 +1,12 @@
 (function(){
     "use strict"
 
-    var target = $('body section.create_account_area');
+    var target = $('section.create_account_area').offset().top;
 
-    $("html, body").animate( { scrollTop: target.height() - 280 } );
-
-    let altera_dados = StorageGetItem('altera_dados');
+    $("html, body").animate( { scrollTop: target } );
+    
     $('#codigo').inputmask('999-999', {autoUnmask: true});
-
+    $('#novo_celular').inputmask('(99) 9 9999-9999');
 
     $('#nome').prop('disabled', true);
     $('#nome').css("background-color", "#cccccc"); 
@@ -25,12 +24,14 @@
         $(this).select();
     });
 
+    let altera_dados = StorageGetItem('altera_dados');
     if(altera_dados == null){
         $('#etapa1').show();
         $('#etapa2').hide();
     }else{
         $('#etapa1').hide();
         $('#etapa2').show();
+        $('#codigo').focus();
     }
 
     if(!Logado()){
@@ -83,7 +84,7 @@
             success: function (result, textStatus, request) {
                 try {
                     StorageSetItem('altera_dados', 'etapa2');
-                    Mensagem(result.mensagem, 'success');
+                    Mensagem(result.mensagem, 'success', function(){ $('#codigo').focus(); });
                     $('#etapa1').hide();
                     $('#etapa2').show();
 
@@ -126,7 +127,19 @@
             success: function (result, textStatus, request) {
                 try {
                     StorageRemoveItem('altera_dados');
-                    Mensagem(result.mensagem, 'success', function(){ Redirecionar('autenticacao.html'); });
+                    Mensagem(result.mensagem, 'success');
+
+                    $('#email').val(novo_email_i != '' ? novo_email_i : $('#email').val());
+                    $('#celular').val(novo_celular_i != '' ? novo_celular_i : $('#celular').val());
+
+                    $('#codigo').val('');
+                    $('#novo_email').val('');
+                    $('#novo_celular').val('');
+
+                    $('#etapa1').show();
+                    $('#etapa2').hide();
+
+
                 } catch (error) {
                     Mensagem(JSON.stringify(result), 'success');
                 }
@@ -135,8 +148,6 @@
                 if (!MensagemErroAjax(request, errorThrown)) {
                     try {
                         var obj = $.parseJSON(request.responseText)
-                        console.log(obj);
-                        return;
                         Mensagem(obj.mensagem, 'warning', function () { $("#" + obj.campo.toLowerCase()).select(); });
                     } catch (error) {
                         Mensagem(request.responseText, 'error', function () { $("#" + obj.campo.toLowerCase()).select(); });
@@ -145,5 +156,4 @@
             }
         });
     })
-
 })(jQuery)
