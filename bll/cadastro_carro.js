2aa218011d8bd4eb604e8fe0705f8e6758c42014
Maike <43520476+Maikedef@users.noreply.github.com>
2022-04-01 22:28:32 -0300

    Implementado geração de campos dinâmicos.

--- bll/cadastro_carro.js
+++ bll/cadastro_carro.js
@@ -11,27 +11,13 @@ var CadastroCarro = {
     Inicializar(){
         this.ObterMarcas();
         this.ObterTodasEspecificacoes();
-        
-        // TODO 
-        this.ObterTipoDeDirecao();
-        this.ObterAssentos();
-        this.ObterCategorias();
-        this.ObterPortas();
-        this.ObterTamanhosMotor();
-        this.ObterTransmissao();
-        this.ObterFreiosDianteiro();
-        this.ObterFreiosTraseiro();
-        this.ObterSuspensoesDianteira();
-        this.ObterSuspensoesTraseira();
-        this.ObterTamanhosRoda();
-        this.ObterTiposSegurancaProtecao();
+        this.ObterTagsDetalhes();
 
         this.EventChangeMarca();
         this.EventChangeModelo();
         this.EventChangeVersao();
         this.EventChangeAnoModelo();
         this.EventChangeAnoFabricacao();
-        this.EventChangeCores();
         this.EventClickVoltarEtapa();
         this.EventClickProxEtapa();
 
@@ -39,13 +25,12 @@ var CadastroCarro = {
         this.spa.find('.combo_versoes').hide();
         this.spa.find('.combo_ano_modelo').hide();
         this.spa.find('.combo_ano_fabricacao').hide();
-        this.spa.find('.combo_cores').hide();
         //this.spa.find('.combo_direcao').hide();
 
         this.spa.find('#placa').hide();
         this.spa.find('#cep').hide();
         this.spa.find('#km').hide();
-        this.spa.find('#preco').show();
+        this.spa.find('#preco').hide();
         this.spa.find('#voltar_etapa').hide();
 
         this.spa.find('#placa').inputmask('aaa-999');
@@ -147,24 +132,6 @@ var CadastroCarro = {
         }
     },
 
-    ObterCores: function(){
-        var this_ = this;
-        
-        $.ajax({
-            url: StorageGetItem('api') + '/v1/mobile/especificacoes/carro/valores?chave=cor',
-            type: 'GET', cache: false, async: true, dataType: 'json',
-            contentType: 'application/x-www-form-urlencoded; charset=utf-8',
-            beforeSend: function () {
-            },
-            success: function (result, textStatus, request) {
-                this_.HtmlCores(result);
-            },
-            error: function (err) {
-                reject(err) // Reject the promise and go to catch()
-            }
-        });
-    },
-
     ObterTodasEspecificacoes : function(){
         var this_ = this;
         $.ajax({
@@ -174,97 +141,29 @@ var CadastroCarro = {
             beforeSend: function () {
             },
             success: function (result, textStatus, request) {
-                this_.especificaoes = result;
+                this_.HtmlEspecificacoes(result);
             },
             error: function (err) {
                 reject(err) // Reject the promise and go to catch()
             }
         });
-        
     },
 
-    ObterDetalhes: function(){
-
-    },
-
-    ObterEspecificacoesChave: function(chave){
-        return this.especificaoes.filter(x => x.chave == chave);
-    },
-
-    ObterEspecificacoesTipo: function(tipo_id){
-        return this.especificaoes.filter(x => x.tipo_id == tipo_id);
-    },
-
-    ObterTipoDeDirecao : function(){
-        let chave = 'Direção';
-        let result = this.ObterEspecificacoesChave(chave);
-        this.HtmlDirecoes(result);
-    },
-
-    ObterAssentos : function(){
-        let chave = 'Assentos';
-        let result = this.ObterEspecificacoesChave(chave);
-        this.HtmlAssento(result);
-    },
-
-    ObterCategorias: function(){
-        let chave = 'Categoria';
-        let result = this.ObterEspecificacoesChave(chave);
-        this.HtmlCategorias(result);
-    },
-
-    ObterPortas : function(){
-        let chave = 'Portas';
-        let result = this.ObterEspecificacoesChave(chave);
-        this.HtmlPortas(result);
-    },
-
-    ObterTamanhosMotor : function(){
-        let chave = 'Tamanho do Motor';
-        let result = this.ObterEspecificacoesChave(chave);
-        this.HtmlTamanhosMotor(result);
-    },
-
-    ObterTransmissao : function() {
-        let chave = 'Transmissão';
-        let result = this.ObterEspecificacoesChave(chave);
-        this.HtmlTransmicao(result);
-    },
-
-    ObterFreiosDianteiro : function(){
-        let chave = 'Freios Dianteiro';
-        let result = this.ObterEspecificacoesChave(chave);
-        this.HtmlFreiosDianteiro(result);
-    },
-
-    ObterFreiosTraseiro : function(){
-        let chave = 'Freios Traseiro';
-        let result = this.ObterEspecificacoesChave(chave);
-        this.HtmlFreiosTraseiro(result);
-    },
-
-    ObterSuspensoesDianteira : function(){
-        let chave = 'Suspensão dianteira';
-        let result = this.ObterEspecificacoesChave(chave);
-        this.HtmlSuspensoesDianteira(result);
-    },
-
-    ObterSuspensoesTraseira : function(){
-        let chave = 'Suspensão traseira';
-        let result = this.ObterEspecificacoesChave(chave);
-        this.HtmlSuspensoesTraseira(result);
-    },
-
-    ObterTamanhosRoda : function(){
-        let chave = 'Tamanho da roda';
-        let result = this.ObterEspecificacoesChave(chave);
-        this.HtmlTamanhosRoda(result);
-    },
-
-    ObterTiposSegurancaProtecao : function(){
-        let tipo_id = 5;
-        let result = this.ObterEspecificacoesTipo(5);
-        this.HtmlInputsChecks(result);
+    ObterTagsDetalhes : function(){
+        var this_ = this;
+        $.ajax({
+            url: StorageGetItem('api') + '/v1/mobile/carros/tags',
+            type: 'GET', cache: false, async: true, dataType: 'json',
+            contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+            beforeSend: function () {
+            },
+            success: function (result, textStatus, request) {
+                this_.HtmlTagsDetalhes(result);
+            },
+            error: function (err) {
+                reject(err) // Reject the promise and go to catch()
+            }
+        });
     },
     
     HtmlComboEspecificada : function(result, classeCombo, opcaoPadrao, tipoCampo){
@@ -336,93 +235,55 @@ var CadastroCarro = {
         this_.HtmlComboEspecificada(result, '.combo_ano_fabricacao', 'Selecione o ano de fabricação', 'string');
     },
 
-    HtmlCores : function(result){
+    HtmlEspecificacoes : function(result){
         var this_ = this;
-        this_.spa.find('.combo_cores').show();
-        this_.HtmlComboEspecificada(result, '.combo_cores', 'Selecione a cor do veiculo', 'especificacao');
-    },
-
-    HtmlCores : function(result){
-        var this_ = this;
-        this_.spa.find('.combo_cores').show();
-        this_.HtmlComboEspecificada(result, '.combo_cores', 'Selecione a cor do veiculo', 'especificacao');
-    },
-
-    HtmlDirecoes : function(result){
-        var this_ = this;
-        this_.spa.find('.combo_direcao').show();
-        this_.HtmlComboEspecificada(result, '.combo_direcao', 'Selecione a direção', 'especificacao');
-    },
-    
-    HtmlAssento : function(result){
-        var this_ = this;
-        this_.spa.find('.combo_assentos').show();
-        this_.HtmlComboEspecificada(result, '.combo_assentos', 'Selecione a quantidade de acentos', 'especificacao');
-    },
-
-    HtmlCategorias : function(result){
-        var this_ = this;
-        this_.spa.find('.combo_categorias').show();
-        this_.HtmlComboEspecificada(result, '.combo_categorias', 'Selecione a categoria', 'especificacao');
-    },
-
-    HtmlPortas : function(result){
-        var this_ = this;
-        this_.spa.find('.combo_portas').show();
-        this_.HtmlComboEspecificada(result, '.combo_portas', 'Selecione a quantidade de portas', 'especificacao');
-    },
-
-    HtmlTamanhosMotor : function(result){
-        var this_ = this;
-        this_.spa.find('.combo_tamanhos_motor').show();
-        this_.HtmlComboEspecificada(result, '.combo_tamanhos_motor', 'Selecione o tamanho do motor', 'especificacao');
-    },
-
-    HtmlTransmicao : function(result){
-        var this_ = this;
-        this_.spa.find('.combo_transmisao').show();
-        this_.HtmlComboEspecificada(result, '.combo_transmisao', 'Selecione o tipo de transmissão', 'especificacao');
-    },
-
-    HtmlFreiosDianteiro : function(result){
-        var this_ = this;
-        this_.spa.find('.combo_freios_dianteiro').show();
-        this_.HtmlComboEspecificada(result, '.combo_freios_dianteiro', 'Selecione o tipo de freio dianteiro', 'especificacao');
-    },
-
-    HtmlFreiosTraseiro : function(result) {
-        var this_ = this;
-        this_.spa.find('.combo_freios_traseiro').show();
-        this_.HtmlComboEspecificada(result, '.combo_freios_traseiro', 'Selecione o tipo de freio traseiro', 'especificacao');
-    },
-
-    HtmlSuspensoesDianteira : function(result){
-        var this_ = this;
-        this_.spa.find('.combo_suspensoes_dianteira').show();
-        this_.HtmlComboEspecificada(result, '.combo_suspensoes_dianteira', 'Selecione o tipo de suspensão dianteira', 'especificacao');
-    },
-
-    HtmlSuspensoesTraseira : function(result){
-        var this_ = this;
-        this_.spa.find('.combo_suspensoes_traseira').show();
-        this_.HtmlComboEspecificada(result, '.combo_suspensoes_traseira', 'Selecione o tipo de suspensão traseira', 'especificacao');
-    },
-
-    HtmlTamanhosRoda : function(result){
-        var this_ = this;
-        this_.spa.find('.combo_tamanhos_roda').show();
-        this_.HtmlComboEspecificada(result, '.combo_tamanhos_roda', 'Selecione o tamanho da roda', 'especificacao');
-    },
-
-    HtmlInputsChecks : function(result){
         let dados_cadastro_carro = this.spa.find('.dados_cadastro_carro');
         $.each(result, function (i, combo) {
-            dados_cadastro_carro.append(` <div class="creat_account">
-                                            <input type="checkbox" id="f-option">
-                                            <label for="f-option">${combo.chave}</label>
-                                            <div class="check"></div>
-                                          </div>`
-                                        )
+            let idCombo = combo.chave.replace(/ /g, '_').trim();
+            if(combo.valor == 'true' || combo.valor == 'false'){
+                
+                dados_cadastro_carro.append(
+                    `<div class="creat_account">
+                        <input type="checkbox" id="f-option_${combo.id}">
+                        <label for="f-option_${combo.id}">${combo.chave}</label>
+                        <div class="check"></div>
+                    </div>`
+                );
+                                         
+            }else{
+
+                // Se a chave não existe adicionado cria caso contrario somente atualiza
+                let especificacao_tipo = this_.spa.find('#combo_' + idCombo)
+                if(especificacao_tipo.length <= 0){
+                    dados_cadastro_carro.append(`
+                        <div class="form-group col-md-12">
+                            <select id="combo_${idCombo}">
+                            </select>
+                        </div> <br>`);
+                    especificacao_tipo = this_.spa.find('#combo_' + idCombo)
+                    especificacao_tipo.append(`<option value="0">Selecione ${combo.chave}</option>`);
+                    especificacao_tipo.append(`<option value="${combo.id}">${combo.valor}</option>`);
+                    especificacao_tipo.niceSelect();
+                    especificacao_tipo.val(0).niceSelect('update');
+                }else{
+                    especificacao_tipo.append(`<option value="${combo.id}">${combo.valor}</option>`);
+                    especificacao_tipo.niceSelect('update');
+                }
+            }
+        });
+        this_.spa.find('.nice-select').css('width', '100%');
+    },
+
+    HtmlTagsDetalhes : function(result){
+        let dados_cadastro_carro = this.spa.find('.dados_cadastro_carro');
+        $.each(result, function (i, combo) {
+            dados_cadastro_carro.append(
+                `<div class="creat_account">
+                    <input type="checkbox" id="f-option_${combo.id}">
+                    <label for="f-option_${combo.id}">${combo.nome}</label>
+                    <div class="check"></div>
+                </div>`
+            );
         });
     },
 
@@ -461,17 +322,12 @@ var CadastroCarro = {
     EventChangeAnoFabricacao : function(){
         var this_ = this;
         $(document).on('change', '.combo_ano_fabricacao', function(event) {
-            this_.ObterCores();
-        });
-    },
-
-    EventChangeCores : function(){
-        var this_ = this;
-        $(document).on('change', '.combo_cores', function(event) {
+            
             this_.spa.find('#placa').show();
             this_.spa.find('#cep').show();
             this_.spa.find('#km').show();
             this_.spa.find('#preco').show();
+            // this_.ObterCores();
         });
     },
 
@@ -490,15 +346,29 @@ var CadastroCarro = {
         var this_ = this;
         $(document).on('click', '#prox_etapa', function(event) {
             event.preventDefault();
+
+            let marcaSelecionada = this_.spa.find('.combo_marca');
+            let modeloSelecionado = this_.spa.find('.combo_modelo');
+            let versaoSelecionada = this_.spa.find('.combo_versoes');
+            let anoModeloSelecionado = this_.spa.find('.combo_ano_modelo');
+            let anoFabricacaoSelecionado = this_.spa.find('.combo_ano_fabricacao');
+
+            /*
+            if(){
+
+            }
+
+            /*
             this_.spa.find('#voltar_etapa').show();
 
             if(this_.etapa == 1) {
 
             }else if(this_.etapa == 2){
                 
-            
+                
             }
             ++this_.etapa;
+            */
         });
     }
 };
