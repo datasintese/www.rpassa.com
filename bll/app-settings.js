b6566c67f38218c018fde79e3f22b580fe2c12bc
Maike <43520476+Maikedef@users.noreply.github.com>
2022-05-04 15:35:39 -0300

    Atualizado servidor da api do rpassa

--- bll/app-settings.js
+++ bll/app-settings.js
@@ -2,10 +2,21 @@
 // localStorage.setItem("api", "http://localhost:5577/website");
 // localStorage.setItem("auth", "http://localhost:5577/oauth/token");
 
+// Amazon
 // Sandbox
+/*
 localStorage.setItem("api", "http://ec2-52-23-232-77.compute-1.amazonaws.com:5576");
 localStorage.setItem("auth", "http://ec2-52-23-232-77.compute-1.amazonaws.com:5576/oauth/token");
+*/
+// -----------------------------------------------------------------------------------------------
 
+// datasintese
+// Sandbox
+
+localStorage.setItem("api", "https://api.datasintese.com:6577");
+localStorage.setItem("auth", "https://api.datasintese.com:6577/oauth/token");
+
+// -----------------------------------------------------------------------------------------------
 //Production
 //localStorage.setItem("api", "https://api.datasintese.com/website");
 //localStorage.setItem("auth", "https://api.datasintese.com/oauth/token");
